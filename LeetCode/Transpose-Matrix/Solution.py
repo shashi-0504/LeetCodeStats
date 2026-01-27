91544d63fb731f2ae5265a0861241d466ba34848
SHASHIKANTH RACHAKONDA <22a31a0552@pragati.ac.in>
2026-01-27 19:36:48 +0530

    Add solution for Transpose Matrix

---
+++ LeetCode/Transpose-Matrix/Solution.py
@@ -0,0 +1,8 @@
+1class Solution(object):
+2    def transpose(self, matrix):
+3        """
+4        :type matrix: List[List[int]]
+5        :rtype: List[List[int]]
+6        """
+7        l=[list(num) for num in zip(*matrix)]
+8        return l
