64ba023c9574e63c4d7dcfd1de587cc9a8197970
SHASHIKANTH RACHAKONDA <22a31a0552@pragati.ac.in>
2026-01-27 20:05:10 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.py
+++ LeetCode/Two-Sum/Solution.py
@@ -1,10 +1,10 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap={}
-        for i,n in enumerate(nums):
-            d=target-n
-            if d in hashmap:
-                return([hashmap[d],i])
-            hashmap[n]=i
-        
-            
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        hashmap={}
+4        for i,n in enumerate(nums):
+5            d=target-n
+6            if d in hashmap:
+7                return([hashmap[d],i])
+8            hashmap[n]=i
+9        
+10            
