a1d1b633bdf5fc66ca81c7a10031c097dadcfa74
SHASHIKANTH RACHAKONDA <22a31a0552@pragati.ac.in>
2026-01-27 19:53:56 +0530

    Add solution for Spiral Matrix

--- LeetCode/Spiral-Matrix/Solution.py
+++ LeetCode/Spiral-Matrix/Solution.py
@@ -1,7 +1,7 @@
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        ans=[]
-        while matrix:
-            ans+=matrix.pop(0)
-            matrix=(list(zip(*matrix)))[::-1]
-        return ans
+1class Solution:
+2    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+3        ans=[]
+4        while matrix:
+5            ans+=matrix.pop(0)
+6            matrix=(list(zip(*matrix)))[::-1]
+7        return ans
