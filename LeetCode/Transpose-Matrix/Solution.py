5551433e490e215103a801df61996c5c3b0439d7
SHASHIKANTH RACHAKONDA <22a31a0552@pragati.ac.in>
2026-03-20 23:08:49 +0530

    Add solution for Transpose Matrix

--- LeetCode/Transpose-Matrix/Solution.py
+++ LeetCode/Transpose-Matrix/Solution.py
@@ -4,5 +4,9 @@
 4        :type matrix: List[List[int]]
 5        :rtype: List[List[int]]
 6        """
-7        l=[list(num) for num in zip(*matrix)]
-8        return l
+7        r, c = len(matrix), len(matrix[0])
+8        t=[[0]*r for _ in range(c)]
+9        for i in range(len(matrix)):
+10            for j in range(len(matrix[0])):
+11                t[j][i]=matrix[i][j]
+12        return t
